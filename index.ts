57c887e20f2fd902d6e71cdf9e3c3137871579d8
Artur Serra <arturserra@pop-os.localdomain>
2022-02-21 14:48:00 +0400

    Created the basic API call and fetching the order info from Amazon.ae

---
+++ index.ts
@@ -0,0 +1,32 @@
+const express = require("express");
+const request = require("request-promise");
+
+const app = express();
+const PORT =  process.env.PORT || 5000;
+
+const apiKey = "089dafaeab51ea38460dc92f1c63652e";
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
+app.use(express.json());
+
+// The initial root route for the API.
+app.get("/", (req: any, res: { send: (arg0: string) => void; }) => {
+    res.send("API Working");
+})
+
+// Fetch product details
+app.get("/products/:productId", async (req: { params: { productId: any; }; }, res: { json: (arg0: any) => void; }) => {
+    const { productId  } = req.params
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.ae/dp/${productId}`);
+
+        res.json (JSON.parse(response));
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Server up and running! 🚀")
+});
