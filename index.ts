21c043378057c9a1081efefd2df73e7c07e3f359
Artur Serra <arturserra@pop-os.localdomain>
2022-02-22 13:21:41 +0400

    Added the endpoint to get reviews based on product Id

--- index.ts
+++ index.ts
@@ -14,14 +14,27 @@ app.get("/", (req: any, res: { send: (arg0: string) => void; }) => {
     res.send("API Working");
 })
 
-// Fetch product details
+// GET product details
 app.get("/products/:productId", async (req: { params: { productId: any; }; }, res: { json: (arg0: any) => void; }) => {
     const { productId  } = req.params
 
     try {
-        const response = await request(`${baseUrl}&url=https://www.amazon.ae/dp/${productId}`);
+        const response = await request(`${ baseUrl }&url=https://www.amazon.ae/dp/${ productId }`);
 
-        res.json (JSON.parse(response));
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// GET products reviews
+app.get("/products/:productId/reviews", async (req: { params: { productId: any; }; }, res: { json: (arg0: any) => void; }) => {
+    const { productId } = req.params
+
+    try {
+        const response = await request(`${ baseUrl }&url=https://www.amazon.ae/product-reviews/${ productId }`)
+
+        res.json(JSON.parse(response))
     } catch (error) {
         res.json(error)
     }
