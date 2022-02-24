3a9edecbc2ff6e332224b02d5510c5e9e3f5aa38
Artur Serra <arturserra@pop-os.localdomain>
2022-02-24 13:10:44 +0400

    Removed the apiKey from the call, replaced for env files

--- index.ts
+++ index.ts
@@ -4,8 +4,13 @@ const request = require("request-promise");
 const app = express();
 const PORT =  process.env.PORT || 5000;
 
-const apiKey = "089dafaeab51ea38460dc92f1c63652e";
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+interface RequestParams {
+
+}
+
+// const apiKey = "089dafaeab51ea38460dc92f1c63652e";
+
+const generateUrl = (apiKey: any) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
@@ -15,11 +20,12 @@ app.get("/", (req: any, res: { send: (arg0: string) => void; }) => {
 });
 
 // GET product details
-app.get("/products/:productId", async (req: { params: { productId: any; }; }, res: { json: (arg0: any) => void; }) => {
+app.get("/products/:productId", async (req: { params: { productId: any; }, query: any; }, res: { json: (arg0: any) => void; }) => {
     const { productId  } = req.params
+    const { api_key } = req.query
 
     try {
-        const response = await request(`${ baseUrl }&url=https://www.amazon.ae/dp/${ productId }`);
+        const response = await request(`${ generateUrl(apiKey) }&url=https://www.amazon.ae/dp/${ productId }`);
 
         res.json(JSON.parse(response));
     } catch (error) {
@@ -28,11 +34,12 @@ app.get("/products/:productId", async (req: { params: { productId: any; }; }, re
 });
 
 // GET product reviews
-app.get("/products/:productId/reviews", async (req: { params: { productId: any; }; }, res: { json: (arg0: any) => void; }) => {
+app.get("/products/:productId/reviews", async (req: { params: { productId: any; }, query: any; }, res: { json: (arg0: any) => void; }) => {
     const { productId } = req.params
+    const { api_key } = req.query
 
     try {
-        const response = await request(`${ baseUrl }&url=https://www.amazon.ae/product-reviews/${ productId }`)
+        const response = await request(`${ generateUrl(apiKey) }&url=https://www.amazon.ae/product-reviews/${ productId }`)
 
         res.json(JSON.parse(response))
     } catch (error) {
@@ -41,11 +48,12 @@ app.get("/products/:productId/reviews", async (req: { params: { productId: any; 
 });
 
 // GET product offers
-app.get("/products/:productId/offers", async (req: { params: { productId: any; }; }, res: { json: (arg0: any) => void; }) => {
+app.get("/products/:productId/offers", async (req: { params: { productId: any; }, query: any; }, res: { json: (arg0: any) => void; }) => {
     const { productId } = req.params
+    const { api_key } = req.query
     
     try {
-        const response = await request(`${ baseUrl }&url=https://www.amazon.ae/gp/offer-listing/${ productId }`)
+        const response = await request(`${ generateUrl(apiKey) }&url=https://www.amazon.ae/gp/offer-listing/${ productId }`)
 
         res.json(JSON.parse(response));
     } catch (error) {
@@ -54,17 +62,18 @@ app.get("/products/:productId/offers", async (req: { params: { productId: any; }
 });
 
 // GET search results
-app.get("/search/:searchQuery", async (req: { params: { searchQuery: any; }; }, res: { json: (arg0: any) => void; }) => {
+app.get("/search/:searchQuery", async (req: { params: { searchQuery: any; }, query: any; }, res: { json: (arg0: any) => void; }) => {
     const { searchQuery } = req.params;
+    const { api_key } = req.query
 
     try {
-        const response = await request(`${ baseUrl }&url=https://www.amazon.ae/s?k=${ searchQuery }`);
+        const response = await request(`${ generateUrl(apiKey) }&url=https://www.amazon.ae/s?k=${ searchQuery }`);
 
         res.json(JSON.parse(response));
     } catch (error) {
         res.json(error)
     }
-})
+});
 
 app.listen(PORT, () => {
     console.log("Server up and running! 🚀")
